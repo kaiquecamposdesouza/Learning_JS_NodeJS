c4f6983e6a14e46e5338f17437a9c30a7c1e3156
kaiquecamposdesouza <kaique.souza@aluno.faculdadeimpacta.com.br>
2023-05-30 16:33:01 -0300

    primeiros passos com arrays

---
+++ array-medias.js
@@ -0,0 +1,11 @@
+// const nota1 = 10;
+// const nota2 = 6.5;
+// const nota3 = 8;
+// const nota4 = 7.5;
+
+// const media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+const notasArray = [10, 6.5, 8, 7.5];
+const media =  (notasArray[0] + notasArray[1] + notasArray[2] + notasArray[3]) / notasArray.length;
+
+console.log(media);
---
+++ array-pop.js
@@ -0,0 +1,5 @@
+const notas = [10, 6, 8, 5.5, 10];
+
+notas.pop();// o metodo pop() remove o ultimo elemento deo array.
+console.log(notas);// saida => [10, 6, 8, 5.5].
+
